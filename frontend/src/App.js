17fc7dc6e065806586e54bbffa85f92b3ee6a3ef
Michael Njoroge <56773318+ngahika01@users.noreply.github.com>
2021-12-04 19:09:24 +0300

    Added minor fixes

---
+++ frontend/src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/registerScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3 ">
+        <Container>
+          <Route path="/login" exact component={LoginScreen} />
+          <Route path="/register" exact component={RegisterScreen} />
+          <Route path="/profile" exact component={ProfileScreen} />
+          <Route path="/shipping" exact component={ShippingScreen} />
+          <Route path="/payment" exact component={PaymentScreen} />
+          <Route path="/placeorder" exact component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/product/:id" exact component={ProductScreen} />
+          <Route path="/cart/:id?" exact component={CartScreen} />
+          <Route path="/admin/userlist" exact component={UserListScreen} />
+          <Route path="/admin/user/:id/edit" exact component={UserEditScreen} />
+          <Route
+            path="/admin/productlist"
+            exact
+            component={ProductListScreen}
+          />
+          <Route
+            path="/admin/productlist/:pageNumber"
+            exact
+            component={ProductListScreen}
+          />
+          <Route
+            path="/admin/product/:id/edit"
+            exact
+            component={ProductEditScreen}
+          />
+          <Route path="/admin/orderlist" exact component={OrderListScreen} />
+          <Route path="/search/:keyword" exact component={HomeScreen} />
+          <Route path="/page/:pageNumber" component={HomeScreen} exact />
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            component={HomeScreen}
+            exact
+          />
+          <Route path="/" component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded my-card">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} className="card-img" variant="top"></Card.Img>
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">Ksh {product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
